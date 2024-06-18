6f5bbc4f9f6897a042faca1f989be16ba6c81e00
dryego <dryego.lisboa@gmail.com>
2024-06-17 21:42:53 -0300

    comfiguracoes.

---
+++ src/main/java/com/starWarsPlanetas/planetasStarWars/PlanetasStarWarsApplication.java
@@ -0,0 +1,13 @@
+package com.starWarsPlanetas.planetasStarWars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetasStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetasStarWarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starWarsPlanetas/planetasStarWars/PlanetasStarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starWarsPlanetas.planetasStarWars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetasStarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
